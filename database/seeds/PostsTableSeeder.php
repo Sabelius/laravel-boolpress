9ff58d4f135d1e10f4d713e4a49d33352ff665b9
Sabelius <sbebby92@virgilio.it>
2021-11-15 15:32:37 +0100

    first commit

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i < 50; $i++){
+            $newPost = new Post();
+            $newPost->author = $faker->name;
+            $newPost->post_creation_date = $faker->date();
+            $newPost->post_name = $faker->words(3, true);
+            $newPost->post_description = $faker->words(12, true);
+            $newPost->content = $faker->text();
+            $newPost->save();
+        }
+    }
+}
