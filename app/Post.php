f24376042d0245345d3c81faeb8cb84670c26df8
Sabelius <sbebby92@virgilio.it>
2021-11-16 15:07:05 +0100

    implementazione foreign key per collegameto categoria a post

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ["author", "post_creation_date", "post_name", "post_description", "content"];
+
+    public function category(){
+        return $this->belongsTo("App\Category");
+    }
 }
