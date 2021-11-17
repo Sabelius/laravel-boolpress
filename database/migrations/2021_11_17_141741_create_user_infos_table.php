2b23bd6f902b184720da52d972ee91b885b844f0
Sabelius <sbebby92@virgilio.it>
2021-11-17 15:31:41 +0100

    inizio relazione one to one

---
+++ database/migrations/2021_11_17_141741_create_user_infos_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->string("address")->nullable();
+            $table->string("country")->nullable();
+            $table->string("phone")->nullable();
+            $table->timestamps();
+
+            $table->foreign("user_id")
+                ->references("id")
+                ->on("users")
+                ->onDelete("set null");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("users", function(Blueprint $table){
+            $table->dropForeign("users_user_id_foreign");
+            $table->dropColumn("user_id");
+
+        });
+        
+        Schema::dropIfExists('user_infos');
+    }
+}
